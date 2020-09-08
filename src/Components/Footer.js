414eee2579a164aba83bb5935da3ba02ea8464aa
Your name here <kr.shampa@yahoo.com>
2020-09-08 01:41:47 -0500

    adding project

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,12 +19,12 @@ class Footer extends Component {
            </ul>
 
            <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
+            <li></li>
            </ul>
 
            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy;   2020 Sultana</li>
+              <li><a title="Styleshout" href="http://www.styleshout.com/"></a></li>
            </ul>
 
         </div>
